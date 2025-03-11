3e66589cb40bb4fabb666c6218ec7342611cf938
PrimWong <65888725+Prim9000@users.noreply.github.com>
2025-03-11 20:40:11 +0700

    bpg

---
+++ data.js
@@ -0,0 +1,483 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20250224_083127_00_384",
+      "name": "IMG_20250224_083127_00_384",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.30782006209635426,
+        "pitch": 0.16939660139025747,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.585076271622375,
+          "pitch": 0.3571964080457306,
+          "rotation": 0,
+          "target": "1-img_20250224_090720_00_390"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6768428680178378,
+          "pitch": 0.2601067897824869,
+          "title": "Traditional Karen Weaving",
+          "text": "The traditional Weaving Machine from the natural resources. Women's center of weaving in the community."
+        }
+      ]
+    },
+    {
+      "id": "1-img_20250224_090720_00_390",
+      "name": "IMG_20250224_090720_00_390",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6157929908436905,
+          "pitch": 0.052165746003675295,
+          "rotation": 0,
+          "target": "2-img_20250224_090746_00_391"
+        },
+        {
+          "yaw": 1.6606113376577234,
+          "pitch": 0.12234332816755256,
+          "rotation": 0,
+          "target": "0-img_20250224_083127_00_384"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6627953673735902,
+          "pitch": 0.12299207084692299,
+          "title": "Weaving Community center",
+          "text": "Weaving Center for the community"
+        }
+      ]
+    },
+    {
+      "id": "2-img_20250224_090746_00_391",
+      "name": "IMG_20250224_090746_00_391",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5076803292035894,
+          "pitch": 0.07830598586289028,
+          "rotation": 0,
+          "target": "1-img_20250224_090720_00_390"
+        },
+        {
+          "yaw": -1.9292299744088641,
+          "pitch": 0.3537028487527021,
+          "rotation": 0,
+          "target": "3-img_20250224_091117_00_395"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-img_20250224_091117_00_395",
+      "name": "IMG_20250224_091117_00_395",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5421208053686222,
+          "pitch": 0.3345612847877213,
+          "rotation": 0,
+          "target": "4-img_20250224_091136_00_396"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-img_20250224_091136_00_396",
+      "name": "IMG_20250224_091136_00_396",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.460114424775357,
+          "pitch": 0.2803315958852295,
+          "rotation": 0,
+          "target": "5-img_20250224_091150_00_397"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-img_20250224_091150_00_397",
+      "name": "IMG_20250224_091150_00_397",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3077502848595799,
+          "pitch": 0.2586159492634241,
+          "rotation": 0,
+          "target": "6-img_20250224_091216_00_398"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-img_20250224_091216_00_398",
+      "name": "IMG_20250224_091216_00_398",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.022004692195721987,
+          "pitch": 0.27861222826573595,
+          "rotation": 0,
+          "target": "7-img_20250224_091248_00_399"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-img_20250224_091248_00_399",
+      "name": "IMG_20250224_091248_00_399",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8134811288781245,
+          "pitch": 0.05940466167139746,
+          "rotation": 0,
+          "target": "6-img_20250224_091216_00_398"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-img_20250224_091317_00_400",
+      "name": "IMG_20250224_091317_00_400",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0962371622972444,
+          "pitch": 0.1675989686464785,
+          "rotation": 0,
+          "target": "7-img_20250224_091248_00_399"
+        },
+        {
+          "yaw": -2.620519407336154,
+          "pitch": 0.26452633731945774,
+          "rotation": 0,
+          "target": "9-img_20250224_091352_00_401"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.7272181803097766,
+          "pitch": 0.2367767999563526,
+          "title": "River Next to the village",
+          "text": "The river that is next to the village"
+        },
+        {
+          "yaw": -2.6696886508742974,
+          "pitch": 0.05189950517512543,
+          "title": "Another side of the river",
+          "text": "The another side of the river"
+        }
+      ]
+    },
+    {
+      "id": "9-img_20250224_091352_00_401",
+      "name": "IMG_20250224_091352_00_401",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.3950766874158944,
+        "pitch": 0.673232277549582,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.836981409369545,
+          "pitch": -0.04806399279322804,
+          "rotation": 0,
+          "target": "8-img_20250224_091317_00_400"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.22305035653502436,
+          "pitch": 0.5414837236707406,
+          "title": "River",
+          "text": "The river next to the village is the main source of life."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
